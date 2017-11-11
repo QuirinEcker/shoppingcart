e6c4798c628293b11a2461f205ac49a5657524cf
QuirinEcker <quirin.ecker@bajupa.com>
2017-11-11 18:14:57 +0100

    renamed

--- src/main/java/Ecker_ShoppingCart.java
+++ src/main/java/Ecker_ShoppingCart.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ecker_ShoppingCard {
+public class Ecker_ShoppingCart {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
